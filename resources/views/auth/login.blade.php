3cb26adf5573689cd90a7c22bff960089913060d
geelat0 <angelicamae.bonganay@gmail.com>
2024-07-31 17:05:54 +0800

    modified UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,15 +62,15 @@
     <div class="authentication-wrapper authentication-cover">
       <div class="authentication-inner row m-0">
         <!-- /Left Text -->
-        <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center p-5">
+        <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center p-5" style="background-color: white">
           <div class="w-100 d-flex justify-content-center">
             <img
-              src="../../assets/img/illustrations/boy-with-rocket-light.png"
+              src="../../assets/img/illustrations/girl-with-laptop-light.png"
               class="img-fluid"
               alt="Login image"
               width="700"
-              data-app-dark-img="illustrations/boy-with-rocket-dark.png"
-              data-app-light-img="illustrations/boy-with-rocket-light.png" />
+              data-app-dark-img="illustrations/girl-with-laptop-dark.png"
+              data-app-light-img="illustrations/girl-with-laptop-light.png" />
           </div>
         </div>
         <!-- /Left Text -->
